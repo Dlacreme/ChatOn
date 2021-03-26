03e59ecb967ecd4f5eb6f988de88a0109996e0a4
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2021-03-26 20:12:49 +0100

    use expired_at instead of inserted_at for token

--- apps/chaton/lib/chaton/auth/user_token.ex
+++ apps/chaton/lib/chaton/auth/user_token.ex
@@ -24,7 +24,8 @@ defmodule Chaton.Auth.UserToken do
   """
   def build_session_token(user) do
     token = :crypto.strong_rand_bytes(@rand_size)
-    {token, %__MODULE__{token: token, context: "session", user_id: user.id}}
+    {token, %__MODULE__{token: token, context: "session", user_id: user.id,
+      expired_at: get_expired_at(@session_validity_in_days)}}
   end
 
   @doc """
@@ -33,10 +34,11 @@ defmodule Chaton.Auth.UserToken do
   The query returns the user found by the token.
   """
   def verify_session_token_query(token) do
+    today = NaiveDateTime.utc_now()
     query =
       from token in token_and_context_query(token, "session"),
         join: user in assoc(token, :user),
-        where: token.inserted_at > ago(@session_validity_in_days, "day"),
+        where: ^today <= token.expired_at,
         select: user
 
     {:ok, query}
@@ -47,6 +49,12 @@ defmodule Chaton.Auth.UserToken do
   """
   def token_and_context_query(token, context) do
     from __MODULE__, where: [token: ^token, context: ^context]
+  end
+
+  defp get_expired_at(validity_day) do
+    NaiveDateTime.utc_now
+    |> NaiveDateTime.truncate(:second)
+    |> NaiveDateTime.add(60 * 60 * 24 * validity_day)
   end
 
 end
