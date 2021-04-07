5b5ba0f2e7e19b3e918e0dac512a8e962d7e4ad8
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2021-04-07 22:47:10 +0200

    search for users is ok

--- apps/chaton/lib/chaton/chat.ex
+++ apps/chaton/lib/chaton/chat.ex
@@ -2,4 +2,16 @@ defmodule Chaton.Chat do
   @moduledoc """
   This module handles the chat rooms as well as the direct messages
   """
+  import Ecto.Query
+
+  @doc """
+  Returns all potential contacts. @query allows to filters using metadata
+  """
+  def list_users(q) when q == nil or q == "" do
+    {:ok, Chaton.Repo.all(from(u in Chaton.Auth.User))}
+  end
+
+  def list_users(query) do
+    Chaton.Auth.User.filter_by_metadata(query)
+  end
 end
---
+++ apps/chaton/test/chaton/chat_test.exs
@@ -0,0 +1,34 @@
+defmodule Chaton.ChatTest do
+  use ExUnit.Case
+  alias Chaton.Repo
+  alias Chaton.Auth.User
+  alias Chaton.Chat
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    {:ok, _} = Repo.insert(%User{} |> User.changeset_meta(%{toto: "tata"}))
+    {:ok, _} = Repo.insert(%User{} |> User.changeset_meta(%{hello: "world"}))
+    :ok
+  end
+
+  test "should filter users" do
+    {:ok, users} = Chat.list_users("world")
+    assert length(users) == 1
+  end
+
+  test "should list contacts" do
+    assert {:ok, _} = Chat.list_users(nil)
+  end
+
+  test "should create a new room" do
+    assert true == false
+  end
+
+  test "should post a message to chat room" do
+    assert true == false
+  end
+
+  test "should send a direct message to a user" do
+    assert true == false
+  end
+end
